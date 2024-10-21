1209157fd0042a9477a0b48cc219c26b93b1adf1
kishlak1ove <nikita.lazarev.2020@bk.ru>
2024-10-21 14:59:32 +0300

    initial commit

---
+++ front_end/src/components/header.js
@@ -0,0 +1,21 @@
+<header>
+    <div class="container_header">
+        <div class="line_header">
+            <div class="logo_header">
+                <div class="img_logo_header"></div>
+                <div class="name_logo_header">
+                    <h1 class="name_text_header">Monopoly</h1>
+                </div>
+            </div>
+            <div class="buttons_header">
+                <a class="header_button_" href="">Поиск игр</a>
+                <a class="header_button" href="">Просмотр игр</a>
+                <a class="header_button" href="">Магазин</a>
+                <a class="header_button" href="">Достижения</a>
+            </div>
+            <div class="login_button_header">
+                <a class="header_button_" id="log" href="">Войти</a>
+            </div>
+        </div>
+    </div>
+</header>
