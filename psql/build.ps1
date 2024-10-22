c363fb5bc7ae12b3da02cb20b3894c168b8b3500
dmitry_bassarab <dmitry.bassarab@yandex.ru>
2024-10-22 12:12:38 +0300

    Refactoring & add new files & syncing with GitLab

--- psql/build.ps1
+++ psql/build.ps1
@@ -1,12 +1,8 @@
 # Usually this is the version (latest, ...)
 $tag = "latest"
 
-# Name of the image
-# in format "<dockerhub-account-name>/<imange-name>"
-# or "ghcr.io/<github-account-name>/<imange-name>" 
-# (if you want to push to GitHub Container Registry
-# instead of Docker Hub)
-$name = "bvlp/psql"
+# Name of the image for GitHub Packages
+$name = "ghcr.io/bvlp/monopoly/psql"
 
 # Build as tagged image
 (Write-Host "Starting build for $name ..." -ForegroundColor Cyan) `
