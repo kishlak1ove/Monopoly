d9aa4f07d9f4349b64c1d8f7c3062bc56bcdd86c
Engineer-Samner <valerapan2831@gmail.com>
2024-11-19 03:27:02 +0300

    update: небольшие изменения

--- backend/django/main/urls.py
+++ backend/django/main/urls.py
@@ -1,30 +1,10 @@
-from django.urls import path, include, re_path
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework import permissions
 
-from .routers import *
-
-class CoreSpectacularAPIView(SpectacularAPIView):
-    custom_settings = {
-        'TITLE': 'Admin API',
-        'SCHEMA_PATH_PREFIX': '/api/v1/',
-        'DESCRIPTION': 'Для администрирования',
-        'VERSION': '1.0.0'
-    }
-    patterns = [
-        re_path(r'^api/v1/', include('main.urls')),
-    ]
-    permission_classes = [permissions.AllowAny,]
-    serve_public = True
-
+from .routers import router
 
 urlpatterns = [
-    path('schema/', CoreSpectacularAPIView.as_view(), name='schema'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
-
-    path('', include(router_room.urls)),
-    path('', include(router_player.urls)),
-    path('', include(router_game.urls)),
-    path('', include(router_realty.urls)),
-    path('', include(router_invite.urls)),
+    path('', include(router.urls)),
 ]
