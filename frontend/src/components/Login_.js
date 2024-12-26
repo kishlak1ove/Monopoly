abf22b98130abfc2c0961692380987bb3b32a955
kishlak1ove <nikita.lazarev.2020@bk.ru>
2024-12-27 01:26:47 +0300

    CI/CD

--- frontend/src/components/Login_.js
+++ frontend/src/components/Login_.js
@@ -32,7 +32,6 @@ export default function Login_() {
                 password
             });
 
-            // Сохранение токенов
             localStorage.setItem('access_token', response.data.access_token);
             localStorage.setItem('refresh_token', response.data.refresh_token);
             localStorage.setItem('user_id', response.data.user_id);
